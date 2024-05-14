43b0c60a104ee54415a3b33be2360605c15bb2e9
Pain-de-mie12 <sofianegremont12@gmail.com>
2024-05-14 20:34:06 +0200

    mise à jour

--- docs/script.js
+++ docs/script.js
@@ -94,7 +94,7 @@ function displayImage2() {
         selectedImage.src = "image/bean_signature.png";
         selectElement.style.display = "none";
     } if (selectedOption === "option2") {
-        selectedImage.src = "";
+        selectedImage.src = "image/leroy_signature.png";
         selectElement.style.display = "none";
     } else if (selectedOption === "expediteur") {
         selectedImage.src = "";
