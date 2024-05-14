1fdaaf89a234a96e3d75fdd3c8715c8d571aecde
Pain-de-mie12 <sofianegremont12@gmail.com>
2024-05-14 20:56:02 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -94,7 +94,7 @@ function displayImage2() {
         selectedImage.src = "image/bean_signature.png";
         selectElement.style.display = "none";
     } else if (selectedOption === "option2") {
-        selectedImage.src = "image/leroy_signature.png";
+        selectedImage.src = "";
         selectElement.style.display = "none";
     } else if (selectedOption === "expediteur") {
         selectedImage.src = "";
